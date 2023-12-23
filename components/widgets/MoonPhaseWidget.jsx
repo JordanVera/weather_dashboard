a4c569c3a5bd794869952ed87ef78a6eb4d0866b
Jordan Vera <verawebdevelopment@gmail.com>
2023-12-22 21:17:30 -0600

    Update MoonPhaseWidget styling and add moon phase image

--- components/widgets/MoonPhaseWidget.jsx
+++ components/widgets/MoonPhaseWidget.jsx
@@ -1,7 +1,7 @@
 const MoonPhaseWidget = () => {
   return (
-    <div className="bg-[#1d1e20] h-100 rounded-2xl w-1/4">
-      <h2>MoonPhaseWidget</h2>
+    <div className="bg-[#1d1e20] h-100 rounded-2xl w-1/4 flex flex-col items-center justify-center">
+      <img src="/images/moonPhases/3.png" className="w-32 h-32" alt="" />
     </div>
   );
 };
