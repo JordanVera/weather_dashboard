e4cbeb4734f526d599760be5dbfcfe07c9cc219c
Jordan Vera <verawebdevelopment@gmail.com>
2023-12-22 22:26:39 -0600

    Refactor NewsletterSignup component

--- components/widgets/NewsletterSignup.jsx
+++ components/widgets/NewsletterSignup.jsx
@@ -1,17 +1,24 @@
+import { Input } from '@material-tailwind/react';
+
 const NewsletterSignup = () => {
   return (
-    <div className="w-1/5 border border-red-500 rounded-2xl h-100">
+    <div className="w-1/5  rounded-2xl h-full">
       <img
         src="/images/landscape.jpg"
         className="w-full h-auto rounded-tr-2xl rounded-tl-2xl"
         alt=""
       />
 
-      <div className="p-2">
+      <div className="p-2 bg-[#1d1e20] h-full rounded-bl-2xl rounded-br-2xl">
         <h5 className="text-gray-500 text-xs">Stay Ahead of the weather!</h5>
         <h5 className="text-gray-500 text-xs">
           Join our newsletter now to receive updates daily.
         </h5>
+
+        <input
+          placeholder="Enter Email..."
+          className="w-full my-2 h-[24px] bg-gray-700 rounded-full placeholder-white placeholder:text-xs"
+        />
       </div>
     </div>
   );
